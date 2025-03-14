466f6704ebb1451f572bce2b5a49bbe4c600fffc
iqra <abdikarimiqra@gmail.com>
2025-03-14 11:25:06 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// Code your solution here      // Code your solution here
+findMatching= (drivers, query)=>{
+    return drivers.filter(driver => 
+      driver.toLowerCase()=== query.toLowerCase()
+    );
+  }
+fuzzyMatch= (drivers, query)=>{
+    return drivers.filter(driver => 
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+  }
+matchName= (drivers, query)=>{
+    return drivers.filter(driver => 
+      driver.name.toLowerCase()=== query.toLowerCase()
+    );
+  }
